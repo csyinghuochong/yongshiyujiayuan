b1d8b52b759d5e4fa35a535e0c6924a60edbee69
tangchunguang <394266315@qq.com>
2024-01-19 13:53:44 +0800

    no message

--- GameDemo202035f1c1/Assets/GameSpript/Data/TapTapSdkHelper.cs
+++ GameDemo202035f1c1/Assets/GameSpript/Data/TapTapSdkHelper.cs
@@ -1,21 +1,23 @@
 using System;
 using TapTap.Bootstrap;
 using TapTap.Common;
+using TapTap.Login;
 using UnityEngine;
+using TapTap.AntiAddiction;
+using TapTap.AntiAddiction.Model;
 
 
 public class TapTapSdkHelper : MonoBehaviour
 {
-    private string clientId = "";
-    private string clientToken = "";
-    private string serverUrl = "";
-    private RegionType regionType;
+    private string clientId = "yrvsirol93o27hydc7";
+    private string clientToken = "4lTeqdbgS3FtR5i4UliTIUaAvN1Ga4AtirdUeGTB";
+    private string serverUrl = "https://yrvsirol.cloud.tds1.tapapis.cn";
+    private RegionType regionType = RegionType.CN;
     private bool showAntiAddictionSwitchAccount; //showAntiAddictionSwitchAccount 参数为 bool 类型，表示是否显示切换账号
 
     // Start is called before the first frame update
     void Start()
     {
-
         var config = new TapConfig.Builder()
         .ClientID(clientId)
         .ClientToken(clientToken)
@@ -24,9 +26,32 @@ public class TapTapSdkHelper : MonoBehaviour
         //.AntiAddictionConfig(showAntiAddictionSwitchAccount);
 
         TapBootstrap.Init(config.ConfigBuilder());
-        //Action<int, string> callback = (code, errorMsg) => {
-        //    AntiAddictionHandler(code, errorMsg);
-        //};
+      
+        // 适用于中国大陆
+        TapLogin.Init(clientId);
+        // 适用于其他国家或地区
+        TapLogin.Init(clientId, true, true);
+
+        AntiAddictionConfig config_2 = new AntiAddictionConfig()
+        {
+            gameId = clientId,      // TapTap 开发者中心对应 Client ID
+            showSwitchAccount = false,      // 是否显示切换账号按钮
+        };
+
+        Action<int, string> callback = (code, errorMsg) => {
+            // code == 500;   // 登录成功
+            // code == 1000;  // 用户登出
+            // code == 1001;  // 切换账号
+            // code == 1030;  // 用户当前无法进行游戏
+            // code == 1050;  // 时长限制
+            // code == 9002;  // 实名过程中点击了关闭实名窗
+            //UnityEngine.Debug.LogFormat($"code: {code} error Message: {errorMsg}");
+            this.AntiAddictionHandler( code, errorMsg );
+        };
+
+        AntiAddictionUIKit.Init(config_2, callback);
+        // 如果是 PC 平台还需要额外设置一下 gameId
+        TapTap.AntiAddiction.TapTapAntiAddictionManager.AntiAddictionConfig.gameId = clientId;
     }
 
     private void AntiAddictionHandler(int code, string errorMsg)
