e3f7b2d8c2c9cf895cd598a293d8a3c9f9d7e8cf
tangchunguang <394266315@qq.com>
2024-01-19 16:37:48 +0800

    no message

--- GameDemo202035f1c1/Assets/GameSpript/Data/TapTapSdkHelper.cs
+++ GameDemo202035f1c1/Assets/GameSpript/Data/TapTapSdkHelper.cs
@@ -1,11 +1,13 @@
 using System;
+
+#if UNITY_ANDROID
+
 using TapTap.Bootstrap;
 using TapTap.Common;
 using TapTap.Login;
 using UnityEngine;
 using TapTap.AntiAddiction;
 using TapTap.AntiAddiction.Model;
-
 
 public class TapTapSdkHelper : MonoBehaviour
 {
@@ -14,6 +16,9 @@ public class TapTapSdkHelper : MonoBehaviour
     private string serverUrl = "https://yrvsirol.cloud.tds1.tapapis.cn";
     private RegionType regionType = RegionType.CN;
     private bool showAntiAddictionSwitchAccount; //showAntiAddictionSwitchAccount 参数为 bool 类型，表示是否显示切换账号
+
+    public Action<string> TapLoingHandler;
+    public Action<int, string> AntiAddictionHandler;
 
     // Start is called before the first frame update
     void Start()
@@ -39,37 +44,78 @@ public class TapTapSdkHelper : MonoBehaviour
         };
 
         Action<int, string> callback = (code, errorMsg) => {
+            //实名认证回调
             // code == 500;   // 登录成功
             // code == 1000;  // 用户登出
             // code == 1001;  // 切换账号
             // code == 1030;  // 用户当前无法进行游戏
             // code == 1050;  // 时长限制
             // code == 9002;  // 实名过程中点击了关闭实名窗
-            //UnityEngine.Debug.LogFormat($"code: {code} error Message: {errorMsg}");
+            UnityEngine.Debug.LogFormat($"code: {code} error Message: {errorMsg}");
             this.AntiAddictionHandler( code, errorMsg );
         };
 
         AntiAddictionUIKit.Init(config_2, callback);
         // 如果是 PC 平台还需要额外设置一下 gameId
         TapTap.AntiAddiction.TapTapAntiAddictionManager.AntiAddictionConfig.gameId = clientId;
+    }
+
+    /// <summary>
+    /// taptap登录
+    /// </summary>
+    public async void TapTapLogin()
+    {
+        string tapAccount = string.Empty;
+        var currentUser = TDSUser.GetCurrent();
+        if (null == currentUser)
+        {
+            UnityEngine.Debug.Log("TapTap 当前未登录");
+            // 开始登录
+        }
+        else
+        {
+            UnityEngine.Debug.Log("TapTap 已登录");
+            // 进入游戏
+        }
+        try
+        {
+            UnityEngine.Debug.Log("TapTapLogin222");
+            // 在 iOS、Android 系统下会唤起 TapTap 客户端或以 WebView 方式进行登录
+            // 在 Windows、macOS 系统下显示二维码（默认）和跳转链接（需配置）
+            var tdsUser = await TDSUser.LoginWithTapTap();
+            UnityEngine.Debug.Log($"TapTapLogin333 success1:{tdsUser}");
+            // 获取 TDSUser 属性
+            var objectId = tdsUser.ObjectId;     // 用户唯一标识
+            var nickname = tdsUser["nickname"];  // 昵称
+            var avatar = tdsUser["avatar"];      // 头像
+            UnityEngine.Debug.Log($"TapTapLogin444 success2:{objectId}");
+            tapAccount =  objectId;
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.Log("登录异常");
+            if (e is TapException tapError)  // using TapTap.Common
+            {
+                UnityEngine.Debug.Log($"encounter exception:{tapError.code} message:{tapError.message}");
+                if (tapError.code == (int)TapErrorCode.ERROR_CODE_BIND_CANCEL) // 取消登录
+                {
+                    UnityEngine.Debug.Log("登录取消");
+                }
+            }
+        }
+        TapLoingHandler.Invoke(tapAccount);
+    }
 
 
+    /// <summary>
+    /// taptap实名认证
+    /// </summary>
+    public void AntiAddiction()
+    {
         ///System.Guid.NewGuid();  系统方法生成唯一id
         // 注意唯一标识参数值长度不能超过 64 字符
         string userIdentifier = SystemInfo.deviceUniqueIdentifier;
         AntiAddictionUIKit.Startup(userIdentifier);
-    }
-
-    private void AntiAddictionHandler(int code, string errorMsg)
-    {
-        // code == 500;   // 登录成功
-        // code == 1000;  // 用户登出
-        // code == 1001;  // 切换账号
-        // code == 1030;  // 用户当前无法进行游戏
-        // code == 1050;  // 时长限制
-        // code == 9002;  // 实名过程中点击了关闭实名窗
-
-        UnityEngine.Debug.LogFormat($"code: {code} error Message: {errorMsg}");
     }
 
     // Update is called once per frame
@@ -83,3 +129,5 @@ public class TapTapSdkHelper : MonoBehaviour
         AntiAddictionUIKit.Exit();
     }
 }
+
+#endif
