a3378df66028a082caebd96060fb1be1a0872648
tangchunguang <394266315@qq.com>
2024-04-18 15:13:35 +0800

    no message

--- GameDemo202035f1c1/Assets/GameSpript/UI/InitGame.cs
+++ GameDemo202035f1c1/Assets/GameSpript/UI/InitGame.cs
@@ -12,13 +12,15 @@ public class InitGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         //打开隐私协议
         if (PlayerPrefs.GetString(YinSi.PlayerPrefsYinSi) != YinSi.YinSiValue)
         {
             Obj_HuaWeiYinSi.SetActive(true);
         }
-
+        else
+        {
+            Obj_HuaWeiYinSi.GetComponent<YinSi>().onRequestPermissionsResult("1_1");
+        }
     }
 
     // Update is called once per frame
