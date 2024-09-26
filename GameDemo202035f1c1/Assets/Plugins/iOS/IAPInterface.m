38c82be255a175644a9948d63cdb2334335e4bc8
tangchunguang <394266315@qq.com>
2024-09-26 17:32:21 +0800

    no message

--- GameDemo202035f1c1/Assets/Plugins/iOS/IAPInterface.m
+++ GameDemo202035f1c1/Assets/Plugins/iOS/IAPInterface.m
@@ -23,7 +23,7 @@ const char* jailbreak_tool_pathes[] = {
  
 
 void CheckIphoneYueyu(const char *p){
-   
+    NSLog(@"TCheckIphoneYueyu!");
     int t1 = 0;
     int t2 = 0;
     int t3 = 0;
@@ -44,33 +44,32 @@ void CheckIphoneYueyu(const char *p){
         t3 = 1;
     }
     
+    
     NSString *str = [ NSString stringWithFormat:@"%d_%d_%d", t1, t2, t3 ];
     UnitySendMessage("WWW_Set", "OnRecvYueyu", [str UTF8String] );
 
 }
 
 void CheckIosSignature(const char *p){
-
-    int t1 = 0;
-    int t2 = 0;
-
-    BOOL b1 = [IAPInterface isFromJailbrokenChannel:@"teamid" andString2:@""];
-    BOOL b2 = [IAPInterface checkCodesign:@"teamid"];
+    NSLog(@"CheckIosSignature!");
+    int t1 = [IAPInterface isFromJailbrokenChannel:@"com.guangying.yongshi" andString2:@"com.guangying.yongshi"];
+    int t2 = [IAPInterface checkCodesign:@"3R4T6JWJ98"];
 
 
     NSString *str = [ NSString stringWithFormat:@"%d_%d", t1, t2 ];
     UnitySendMessage("WWW_Set", "OnRecvIosSignature", [str UTF8String] );
 }
 
-+(BOOL)isFromJailbrokenChannel:(NSString*)appid andString2: (NSString*)initbundleid  {
++(int)isFromJailbrokenChannel:(NSString*)appid andString2: (NSString*)initbundleid  {
     NSString *bundleId = [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleIdentifierKey];
     
-    
-    NSLog(@"%@  aaaaaa", bundleId);
+
     if (![bundleId isEqualToString:initbundleid]) {
         
-        return YES;
+        return 1;
     }
+    
+
     //取出embedded.mobileprovision这个描述文件的内容进行判断
     NSString *mobileProvisionPath = [[NSBundle mainBundle] pathForResource:@"embedded" ofType:@"mobileprovision"];
     NSData *rawData = [NSData dataWithContentsOfFile:mobileProvisionPath];
@@ -78,27 +77,36 @@ void CheckIosSignature(const char *p){
     NSRange plistStartRange = [rawDataString rangeOfString:@"<plist"];
     NSRange plistEndRange = [rawDataString rangeOfString:@"</plist>"];
     if (plistStartRange.location != NSNotFound && plistEndRange.location != NSNotFound) {
+       
+       
         NSString *tempPlistString = [rawDataString substringWithRange:NSMakeRange(plistStartRange.location, NSMaxRange(plistEndRange))];
         NSData *tempPlistData = [tempPlistString dataUsingEncoding:NSUTF8StringEncoding];
         NSDictionary *plistDic =  [NSPropertyListSerialization propertyListWithData:tempPlistData options:NSPropertyListImmutable format:nil error:nil];
         
-        NSArray *applicationIdentifierPrefix = [plistDic objectForKey:<#(nonnull id)#>:@"ApplicationIdentifierPrefix" defaultValue:nil];
-        NSDictionary *entitlementsDic = [plistDic objectForKey:<#(nonnull id)#>:@"Entitlements" defaultValue:nil];
-        NSString *mobileBundleID = [entitlementsDic objectForKey:<#(nonnull id)#>:@"application-identifier" defaultValue:nil];
+        NSArray *applicationIdentifierPrefix = [plistDic objectForKey:@"ApplicationIdentifierPrefix" ];
+        NSDictionary *entitlementsDic = [plistDic objectForKey:@"Entitlements" ];
+        NSString *mobileBundleID = [entitlementsDic objectForKey:@"application-identifier"];
+        
+        NSLog(@"%@  bbbbbb1111", mobileBundleID);
+        NSLog(@"%@  bbbbbb2222", [NSString stringWithFormat:@"%@.%@",[applicationIdentifierPrefix firstObject],initbundleid]);
+        
         if (applicationIdentifierPrefix.count > 0 && mobileBundleID != nil) {
-            if (![mobileBundleID isEqualToString:[NSString stringWithFormat:@"%@.%@",[applicationIdentifierPrefix firstObject],@"your applicationId"]]) {
-                return YES;
+            if (![mobileBundleID isEqualToString:[NSString stringWithFormat:@"%@.%@",[applicationIdentifierPrefix firstObject],appid]]) {
+                return 1;
             }
         }
     }
-
-    return NO;
+    
+    return 0;
     
 }
 
-+(BOOL)checkCodesign:(NSString*)initteamID {
++(int)checkCodesign:(NSString*)initteamID {
     //获取描述文件路径
     NSString *embeddedPath = [[NSBundle mainBundle] pathForResource:@"embedded" ofType:@"mobileprovision"];
+    
+    
+    NSLog(@"%@  ccccc11111", embeddedPath);
     if ([[NSFileManager defaultManager] fileExistsAtPath:embeddedPath]) {
         // 读取application-identifier
         NSString *embeddedProvisioning = [NSString stringWithContentsOfFile:embeddedPath encoding:NSASCIIStringEncoding error:nil];
@@ -114,22 +122,26 @@ void CheckIosSignature(const char *p){
                 NSArray *identifierComponents = [fullIdentifier componentsSeparatedByString:@"."];
                 NSString *appIdentifier = [identifierComponents firstObject];
                 
-
-                NSLog(@"%@  ccccc", initteamID);
-                NSLog(@"%@  ddddd", appIdentifier);
+                NSLog(@"%@  ddddd1111", initteamID);
+                NSLog(@"%@  ddddd2222", appIdentifier);
                 
                 // 对比签名ID
                 if ([appIdentifier isEqual:initteamID]) {
                     NSLog(@"签名验证签名验证成功");
-                    return YES;
+                    return 0;
                 } else{
                     NSLog(@"签名验证签名验证失败");
-                    return NO;
+                    return 1;
                 }
                 break;
             }
         }
     }
+    else {
+        NSLog(@"%@  ccccc22222", embeddedPath);
+        return 2;
+    }
+    return  0;;
 }
 
 @end
