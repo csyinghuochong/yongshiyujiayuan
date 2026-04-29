142e143afad3567d3ef11661a27c48953d6a1668
FortuneCat0v0 <1142094319@qq.com>
2026-04-29 16:41:18 +0800

    热更流程跑通！

--- GameDemo202035f1c1/Assets/GameScripts/Loader/PatchLogic/FsmNode/FsmInitializePackage.cs
+++ GameDemo202035f1c1/Assets/GameScripts/Loader/PatchLogic/FsmNode/FsmInitializePackage.cs
@@ -105,7 +105,8 @@ internal class FsmInitializePackage : IStateNode
     private string GetHostServerURL()
     {
         //string hostServerIP = "http://10.0.2.2"; //安卓模拟器地址
-        string hostServerIP = "http://127.0.0.1";
+        // string hostServerIP = "http://127.0.0.1";
+        string hostServerIP = "http://192.168.31.186";
         string appVersion = "v1.0";
 
 #if UNITY_EDITOR
