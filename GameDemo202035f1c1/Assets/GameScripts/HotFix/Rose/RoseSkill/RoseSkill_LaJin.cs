2a616ef307cc51e2d856ec203c138a3a8741ed56
Bacmuz <1638974855@qq.com>
2026-05-20 12:48:52 +0800

    no message

---
+++ GameDemo202035f1c1/Assets/GameScripts/HotFix/Rose/RoseSkill/RoseSkill_LaJin.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+//嘲讽技能
+public class RoseSkill_LaJin : MonoBehaviour
+{
+
+	
+	// Use this for initialization
+	void Start () {
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+	}
+
+    //卸载时调用
+    void OnDestroy()
+    {
+        
+    }
+}
